82f02dc4fe9c10d0837a1caad5110fa35fc85940
MMeggett <117018012+MMeggett@users.noreply.github.com>
2023-01-13 15:17:53 -0500

    initial commit

--- password-generator/Develop/script.js
+++ password-generator/Develop/script.js
@@ -2,10 +2,14 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
- 
+function showOptions() {
+    var list = document.querySelector(".options");
 
+    if (list.style.display == "none") {
+        list.style.display == 'block';
+    }
+    else { list.style.display == "none" }
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", showOptions);
